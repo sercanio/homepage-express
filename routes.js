5332f2f31fceeaf3979f1b481dddfa69afb06f76
Sercan Ateş <104576153+sercanio@users.noreply.github.com>
2023-12-09 21:49:18 +0300

    Initial commit

---
+++ routes.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = function (app, opts) {
+  // Setup routes, middleware, and handlers
+  app.get('/', (req, res) => {
+    res.locals.name = 'My Personal Blog Page'
+    res.render('index')
+  })
+}
