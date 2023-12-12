e251aa7769c165f0e3092ae0c4c142890114956b
Sercan Ateş <104576153+sercanio@users.noreply.github.com>
2023-12-12 19:11:33 +0300

    Add aboutme page

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
         },
       },
     ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "synthwave"]
-    // darkTheme: 'dark', // name of one of the included themes for dark mode
+    // darkTheme: 'light', // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
